0527ef5b14dde4035798ef1e76061202e8002e7f
Olive Ox <oliveox@protonmail.com>
2021-12-07 20:04:27 +0200

    Refactor for modularization. Cleaner code

--- main.js
+++ main.js
@@ -1,88 +1,87 @@
-const fs = require('fs').promises;
-const sharp = require('sharp');
+const fs = require('fs').promises
+const sharp = require('sharp')
 
-const {getAllRelevantFiles, getFileOrientation, addWatermark} = require('./utils');
+const { getImageRatio, getAllRelevantFiles, getFileOrientation, addWatermark } = require('./utils')
 
 const main = async () => {
-    try {
-        const args = parse_args();
-        if (!args) throw 'Arguments could not be parsed'
+	try {
+		const args = parseArgs()
+		if (!args) throw 'Arguments could not be parsed'
 
-        const dirPath = args.directory;
-        const watermarkPath = args.watermark;
-        const prefix = args.prefix;
-        const outputDirPath = args.output_directory;
+		const dirPath = args.directory
+		const watermarkPath = args.watermark
+		const prefix = args.prefix
+		const outputDirPath = args.output_directory
 
-        if (!pathExists(dirPath)) 
-            throw "Input media files directory path doesn't exist";
+		if (!pathExists(dirPath)) { throw `Input media files directory path [${dirPath}] doesn't exist` }
 
-        if (!pathExists(watermarkPath)) 
-            throw "Watermark file path doesn't exist";
+		if (!pathExists(watermarkPath)) { throw `Watermark file path [${watermarkPath}}] doesn't exist` }
 
-        if (!prefix || prefix.length === 0) 
-            throw "Prefix not specified";
+		if (!prefix || prefix.length === 0) { throw 'Prefix empty or not specified' }
 
-        if (outputDirPath && !(await pathExists(outputDirPath))) {
-            try {
-                await fs.mkdir(outputDirPath, {recursive: true})
-            } catch (err) {
-                throw `Error while trying to created output directory path: ${err}`
-            }
-        }
+		if (outputDirPath && !(await pathExists(outputDirPath))) {
+			try {
+				await fs.mkdir(outputDirPath, { recursive: true })
+			} catch (err) {
+				throw `Error while trying to created output directory path [${outputDirPath}]: ${err}`
+			}
+		}
 
-        // get watermark ratio
-        const image = sharp(watermarkPath);
-        const metadata = await image.metadata();
-        if (!metadata || !metadata.height || !metadata.width)
-            throw `Could not fetch watermark metadata at path [${watermarkPath}]. The end ...`
-        const watermarkImageRatio = metadata.width / metadata.height;    
+		const watermarkImageRatio = await getImageRatio(watermarkPath)
 
-        // get image and video files
-        console.log(`Fetching all relevant files from [${dirPath}]`);
-        const filePaths = await getAllRelevantFiles(dirPath);
-        console.log(`Successfully fetched all relevant files from [${dirPath}]`);
+		// get image and video files
+		console.log(`Scanning [${dirPath}] for relevant files`)
+		const filePaths = await getAllRelevantFiles(dirPath)
+		console.log('Successfully scanned directory')
 
-        console.log(`Adding watermark to files from [${dirPath}]`);
-        for (let filePath of filePaths) {
+		console.log('Adding watermark ... ')
+		for (const filePath of filePaths) {
+			try {
+				const fileOrientation = await getFileOrientation(filePath)
 
-            try {
-                const fileOrientation = await getFileOrientation(filePath);
-    
-                // TODO - pass object not data
-                await addWatermark(filePath, watermarkPath, fileOrientation, prefix, outputDirPath, watermarkImageRatio);
-    
-                console.log(`Watermarked [${filePath}]`);
-            } catch (e) {
-                console.error(`Could not watermark file [${filePath}]. Skipping ... \n Error: ${e}`);
-            }
-           
-        }
-        console.log(`Watermarking finished`);
-        
-    } catch (e) {
-        console.error(e);
-    }
+				await addWatermark({
+					filePath,
+					watermarkPath,
+					fileOrientation,
+					prefix,
+					outputDirPath,
+					watermarkImageRatio
+				})
+
+				console.log(`Watermarked [${filePath}]`)
+			} catch (e) {
+				console.error(`Could not watermark file [${filePath}]. Skipping ... \n Error: ${e}`)
+			}
+		}
+		console.log('Watermarking finished')
+	} catch (e) {
+		console.error(e)
+	}
 }
 
-const parse_args = () => {
-    const { ArgumentParser } = require('argparse');
-    const { version } = require('./package.json');
-    
-    const parser = new ArgumentParser({
-        description: 'Add a watermark to a batch of images and videos.'
-    });
- 
-    parser.add_argument('-v', '--version', { action: 'version', version });
-    parser.add_argument('-d', '--directory', { help: 'Input media files directory path | [Required]' });
-    parser.add_argument('-w', '--watermark', { help: 'Watermark file path | [Required]' });
-    parser.add_argument('-p', '--prefix', { help: 'Prefix of the new file. OutputFilename = {prefix}{InputFilename} | [Required]' });
-    parser.add_argument('-od', '--output_directory', { help: "Output watermarked files drectory. If path doesn't exist, it will be created | [Optional]" });
-    
-    return parser.parse_args();
+const parseArgs = () => {
+	const { ArgumentParser } = require('argparse')
+	const { version } = require('./package.json')
+
+	const parser = new ArgumentParser({
+		description: 'Add a watermark to a batch of images and videos.'
+	})
+
+	parser.add_argument('-v', '--version', { action: 'version', version })
+	parser.add_argument('-d', '--directory', { help: 'Input media files directory path | [Required]' })
+	parser.add_argument('-w', '--watermark', { help: 'Watermark file path | [Required]' })
+	parser.add_argument('-p', '--prefix', {
+		help: 'Prefix of the new file. OutputFilename = {prefix}{InputFilename} | [Required]' 
+	})
+	parser.add_argument('-od', '--output_directory', {
+		help: "Output watermarked files drectory. If path doesn't exist, it will be created | [Optional]" 
+	})
+
+	return parser.parse_args()
 }
 
 const pathExists = async path => !!(await fs.stat(path).catch(e => false));
 
 (async () => {
-    await main();
-})();
+	await main()
+})()
--- utils.js
+++ utils.js
@@ -1,207 +1,216 @@
-const fs = require('fs');
-const path = require('path');
-const fileType = require('file-type');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-const ffprobe = require('ffprobe');
-const ffprobeStatic = require('ffprobe-static');
-const sharp = require('sharp');
+const fs = require('fs')
+const path = require('path')
+const fileType = require('file-type')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const ffprobe = require('ffprobe')
+const ffprobeStatic = require('ffprobe-static')
+const sharp = require('sharp')
 
 const {relevantFileTypes, ORIENTATION, settings,
-     watermarkToHeightRatio, watermarkToWidthRatio, exifPortraitCodes} = require('./config');
+	watermarkToHeightRatio, watermarkToWidthRatio, exifPortraitCodes} = require('./config')
 
-exports.addWatermark = async (filePath, watermarkPath, orientation, prefix, outputDirPath, watermarkImageRatio) => {
+exports.addWatermark = async function ({
+	filePath, watermarkPath, fileOrientation, prefix, outputDirPath, watermarkImageRatio
+}) {
+	const outputFilename = `${prefix}${path.basename(filePath)}`
+	const outputFilePath = outputDirPath
+		? path.join(outputDirPath, outputFilename)
+		: path.join(path.dirname(filePath), outputFilename)
 
-    const outputFilename = `${prefix}${path.basename(filePath)}`;
-    const outputFilePath = outputDirPath ? 
-                            path.join(outputDirPath, outputFilename) : 
-                            path.join(path.dirname(filePath), outputFilename);
+	let overlay = `[wtrmrk]${getOverlay(settings)}`
 
-    let overlay = `[wtrmrk]${getOverlay(settings)}`;
-
-    const {ext, mime} = await fileType.fromFile(filePath);
-    const transpose = orientation == ORIENTATION.PORTRAIT && mime.split("/")[0] !== "video" ?
-                                         "[0:v]transpose=2 [mediaFile]," : '';
+	const { ext, mime } = await fileType.fromFile(filePath)
+	const transpose = fileOrientation === ORIENTATION.PORTRAIT && mime.split('/')[0] !== 'video' ?
+		'[0:v]transpose=2 [mediaFile],' : ''
 
     
-    if (transpose.length > 0) {
-        overlay = `[mediaFile]${overlay}`
-    } else {
-        overlay = `[0:v]${overlay}`;
-    }
+	if (transpose.length > 0) {
+		overlay = `[mediaFile]${overlay}`
+	} else {
+		overlay = `[0:v]${overlay}`
+	}
     
-    const watermarkScaling = await getWatermarkScaling(filePath, orientation, watermarkImageRatio);
+	const watermarkScaling = await getWatermarkScaling(filePath, fileOrientation, watermarkImageRatio)
     
-    let command = `ffmpeg -y -i "${filePath}" -i "${watermarkPath}" -filter_complex "${transpose}${watermarkScaling}${overlay}" "${outputFilePath}"`;
+	let command = `ffmpeg -y -i "${filePath}" -i "${watermarkPath}" -filter_complex "${transpose}${watermarkScaling}${overlay}" "${outputFilePath}"`
     
-    try {
-        await exec(command);
-    } catch (e) {
-        console.debug(`FFmpeg error. Command: [${command}]`);
-        throw `FFmpeg error for file [${filePath}]: [${e}]`;
-    }
+	try {
+		await exec(command)
+	} catch (e) {
+		console.debug(`FFmpeg error. Command: [${command}]`)
+		throw `FFmpeg error for file [${filePath}]: [${e}]`
+	}
 }
 
 const getWatermarkScaling = async (filePath, orientation, watermarkImageRatio) => {
 
-    // good for both images and videos
-    const fileMetadata = await ffprobe(filePath, { path: ffprobeStatic.path });
+	// good for both images and videos
+	const fileMetadata = await ffprobe(filePath, { path: ffprobeStatic.path })
 
-    if (!fileMetadata || !fileMetadata.streams[0].height || !fileMetadata.streams[0].width)
-        throw `Could not get height / width data about [${filePath}]. Skipping ... `
+	if (!fileMetadata || !fileMetadata.streams[0].height || !fileMetadata.streams[0].width)
+		throw `Could not get height / width data about [${filePath}]. Skipping ... `
 
-    const imageWidth = fileMetadata.streams[0].width;
-    const imageHeight = fileMetadata.streams[0].height;
+	const imageWidth = fileMetadata.streams[0].width
+	const imageHeight = fileMetadata.streams[0].height
 
-    let watermarkWidth;
-    let watermarkHeight;
+	let watermarkWidth
+	let watermarkHeight
 
-    switch(orientation) {
-        case ORIENTATION.LANDSCAPE:
-            watermarkHeight = imageHeight * watermarkToHeightRatio;
-            watermarkWidth = watermarkImageRatio * watermarkHeight;
-            break;
-        case ORIENTATION.PORTRAIT:
-            watermarkWidth = imageWidth * watermarkToWidthRatio;
-            watermarkHeight = watermarkWidth / watermarkImageRatio;
-            break;
-        default:
-            throw `Orientation value unknown. Fatal error!`
-    }
+	switch(orientation) {
+	case ORIENTATION.LANDSCAPE:
+		watermarkHeight = imageHeight * watermarkToHeightRatio
+		watermarkWidth = watermarkImageRatio * watermarkHeight
+		break;
+	case ORIENTATION.PORTRAIT:
+		watermarkWidth = imageWidth * watermarkToWidthRatio
+		watermarkHeight = watermarkWidth / watermarkImageRatio
+		break;
+	default:
+		throw 'Orientation value unknown. Fatal error!'
+	}
 
-    return `[1:v] scale=${watermarkWidth}:${watermarkHeight} [wtrmrk];`;
+	return `[1:v] scale=${watermarkWidth}:${watermarkHeight} [wtrmrk];`
 }
 
 
 
 const getOverlay = (settings) => {
-    let overlay = '';
+	let overlay = ''
 
-    if (settings.position == null || !['NE','NC','NW','SE','SC','SW','C','CE','CW'].includes(settings.position))
-		throw `invalid_watermark_position: ${settings.position}`;
+	if (settings.position == null || !['NE','NC','NW','SE','SC','SW','C','CE','CW'].includes(settings.position))
+		throw `invalid_watermark_position: ${settings.position}`
 
-    if (settings.margin_nord == null || isNaN(settings.margin_nord))
-        settings.margin_nord = 0;
-    if (settings.margin_south == null || isNaN(settings.margin_south))
-        settings.margin_south = 0;
-    if (settings.margin_east == null || isNaN(settings.margin_east))
-        settings.margin_east = 0;
-    if (settings.margin_west == null || isNaN(settings.margin_west))
-        settings.margin_west = 0;
+	if (settings.margin_nord == null || isNaN(settings.margin_nord))
+		settings.margin_nord = 0
+	if (settings.margin_south == null || isNaN(settings.margin_south))
+		settings.margin_south = 0
+	if (settings.margin_east == null || isNaN(settings.margin_east))
+		settings.margin_east = 0
+	if (settings.margin_west == null || isNaN(settings.margin_west))
+		settings.margin_west = 0
 
-    const getHorizontalMargins = (east, west) => {
-        return getSing(east, false).toString() + getSing(west, true).toString();
-    }
+	const getHorizontalMargins = (east, west) => {
+		return getSing(east, false).toString() + getSing(west, true).toString()
+	}
     
-    const getVerticalMargins = (nord, south) => {
-        return getSing(nord, false).toString() + getSing(south, true).toString();
-    }
+	const getVerticalMargins = (nord, south) => {
+		return getSing(nord, false).toString() + getSing(south, true).toString()
+	}
 
-    const getSing = (val, inverse) => {
-        return (val > 0 ? (inverse ? '-' : '+') : (inverse ? '+' : '-')).toString() + Math.abs(val).toString();
-    }
+	const getSing = (val, inverse) => {
+		return (val > 0 ? (inverse ? '-' : '+') : (inverse ? '+' : '-')).toString() + Math.abs(val).toString()
+	}
 
-    // Calculate formula		
-    switch (settings.position) {
-        case 'NE':
-            overlay = '0' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':0' + getVerticalMargins(settings.margin_nord, settings.margin_south);
-            break;
-        case 'NC':
-            overlay = 'main_w/2-overlay_w/2' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':0' + getVerticalMargins(settings.margin_nord, settings.margin_south);
-            break;
-        case 'NW':
-            overlay = 'main_w-overlay_w' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':0' + getVerticalMargins(settings.margin_nord, settings.margin_south);
-            break;
-        case 'SE':
-            overlay = '0' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h-overlay_h' + getVerticalMargins(settings.margin_nord, settings.margin_south);
-            break;
-        case 'SC':
-            overlay = 'main_w/2-overlay_w/2' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h-overlay_h' + getVerticalMargins(settings.margin_nord, settings.margin_south);
-            break;
-        case 'SW':
-            overlay = 'main_w-overlay_w' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h-overlay_h' + getVerticalMargins(settings.margin_nord, settings.margin_south);
-            break;
-        case 'CE':
-            overlay = '0' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h/2-overlay_h/2' + getVerticalMargins(settings.margin_nord, settings.margin_south);
-            break;
-        case 'C':
-            overlay = 'main_w/2-overlay_w/2' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h/2-overlay_h/2' + getVerticalMargins(settings.margin_nord, settings.margin_south);
-            break;
-        case 'CW':
-            overlay = 'main_w-overlay_w' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h/2-overlay_h/2' + getVerticalMargins(settings.margin_nord, settings.margin_south);
-            break;
-    }
+	// Calculate formula		
+	switch (settings.position) {
+	case 'NE':
+		overlay = '0' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':0' + getVerticalMargins(settings.margin_nord, settings.margin_south)
+		break;
+	case 'NC':
+		overlay = 'main_w/2-overlay_w/2' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':0' + getVerticalMargins(settings.margin_nord, settings.margin_south)
+		break;
+	case 'NW':
+		overlay = 'main_w-overlay_w' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':0' + getVerticalMargins(settings.margin_nord, settings.margin_south)
+		break;
+	case 'SE':
+		overlay = '0' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h-overlay_h' + getVerticalMargins(settings.margin_nord, settings.margin_south)
+		break;
+	case 'SC':
+		overlay = 'main_w/2-overlay_w/2' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h-overlay_h' + getVerticalMargins(settings.margin_nord, settings.margin_south)
+		break;
+	case 'SW':
+		overlay = 'main_w-overlay_w' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h-overlay_h' + getVerticalMargins(settings.margin_nord, settings.margin_south)
+		break;
+	case 'CE':
+		overlay = '0' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h/2-overlay_h/2' + getVerticalMargins(settings.margin_nord, settings.margin_south)
+		break;
+	case 'C':
+		overlay = 'main_w/2-overlay_w/2' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h/2-overlay_h/2' + getVerticalMargins(settings.margin_nord, settings.margin_south)
+		break;
+	case 'CW':
+		overlay = 'main_w-overlay_w' + getHorizontalMargins(settings.margin_east, settings.margin_west) + ':main_h/2-overlay_h/2' + getVerticalMargins(settings.margin_nord, settings.margin_south)
+		break;
+	}
 
-    return `overlay=${overlay}`;
+	return `overlay=${overlay}`
 }
 
 exports.getFileOrientation = async (filePath) => {
 
-    const {_, mime} = await fileType.fromFile(filePath);
-    const mediaType = mime.split("/")[0];
+	const {_, mime} = await fileType.fromFile(filePath)
+	const mediaType = mime.split('/')[0]
 
-    switch(mediaType) {
-        case "image":
-            const image = sharp(filePath);
-            const metadata = await image.metadata();
+	switch(mediaType) {
+	case 'image':
+		const image = sharp(filePath)
+		const metadata = await image.metadata()
 
-            if (!metadata) `Can't get metadata about [${filePath}]`
+		if (!metadata) `Can't get metadata about [${filePath}]`
 
-            return metadata.orientation && exifPortraitCodes.includes(metadata.orientation) ? ORIENTATION.PORTRAIT : ORIENTATION.LANDSCAPE;
-            break;
-        case "video":
-            const data = await ffprobe(filePath, {path: ffprobeStatic.path});
+		return metadata.orientation && exifPortraitCodes.includes(metadata.orientation) ? ORIENTATION.PORTRAIT : ORIENTATION.LANDSCAPE
+		break;
+	case 'video':
+		const data = await ffprobe(filePath, {path: ffprobeStatic.path})
 
-            if (!data) throw `Can't get metadata about [${filePath}]`
+		if (!data) throw `Can't get metadata about [${filePath}]`
 
-            const rotation = data.streams[0].tags.rotate;
+		const rotation = data.streams[0].tags.rotate
 
-            if (rotation) {
-                return Number.isInteger(rotation / 90) ? ORIENTATION.PORTRAIT : ORIENTATION.LANDSCAPE ;
-            } else if (data.streams[0].height && data.streams[0].width) {
-                return data.streams[0].height > data.streams[0].width ? ORIENTATION.PORTRAIT : ORIENTATION.LANDSCAPE;
-            } else {
-                throw `Can't get enough metadata about [${filePath}] in order to get its orientation`
-            }
-            // empirically speaking, video files without rotation metadata are usually landscape
+		if (rotation) {
+			return Number.isInteger(rotation / 90) ? ORIENTATION.PORTRAIT : ORIENTATION.LANDSCAPE 
+		} else if (data.streams[0].height && data.streams[0].width) {
+			return data.streams[0].height > data.streams[0].width ? ORIENTATION.PORTRAIT : ORIENTATION.LANDSCAPE
+		} else {
+			throw `Can't get enough metadata about [${filePath}] in order to get its orientation`
+		}
+		// empirically speaking, video files without rotation metadata are usually landscape
             
-        default:
-            throw "Irrelevant media type given for analysis";
-            break;
-    }
+	default:
+		throw 'Irrelevant media type given for analysis'
+		break;
+	}
 
 }
 
-getFilesRecursively = async (dirPath, arrayOfFiles) => {
-    files = fs.readdirSync(dirPath)
-  
-    arrayOfFiles = arrayOfFiles || []
-  
-    for (file of files) {
-        if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
-            arrayOfFiles = await getFilesRecursively(path.join(dirPath, file), arrayOfFiles);
-        } else {
-            let filePath = path.join(dirPath, file);
-            if (await fileIsRelevant(filePath)) arrayOfFiles.push(filePath);
-        }
-    }
-  
-    return arrayOfFiles
+async function getFilesRecursively (dirPath, arrayOfFiles) {
+	const files = fs.readdirSync(dirPath)
+	arrayOfFiles = arrayOfFiles || []
+	for (const file of files) {
+		if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
+			arrayOfFiles = await getFilesRecursively(path.join(dirPath, file), arrayOfFiles)
+		} else {
+			const filePath = path.join(dirPath, file)
+			if (await fileIsRelevant(filePath)) arrayOfFiles.push(filePath)
+		}
+	}
+
+	return arrayOfFiles
 }
 
 exports.getAllRelevantFiles = async (dirPath, arrayOfFiles) => {
-    return getFilesRecursively(dirPath, arrayOfFiles);
+	return getFilesRecursively(dirPath, arrayOfFiles)
 }
 
 const fileIsRelevant = async (filePath) => {
-    try {
-        const {ext, mime} = await fileType.fromFile(filePath);
-        const isRelevant = mime && mime.split("/").length == 2 && relevantFileTypes.includes(mime.split("/")[0]) 
-                        ? true : false;
+	try {
+		const {ext, mime} = await fileType.fromFile(filePath)
+		const isRelevant = mime && mime.split('/').length == 2 && relevantFileTypes.includes(mime.split('/')[0]) 
+			? true : false
     
-        return isRelevant;
-    } catch (e) {
-        console.debug(`Can't get media type of file [${filePath}]. Exception: ${e}`);
-        return false;
-    }
+		return isRelevant
+	} catch (e) {
+		console.debug(`Can't get media type of file [${filePath}]. Exception: ${e}`)
+		return false
+	}
+}
+
+exports.getImageRatio = async function (imagePath) {
+	const image = sharp(imagePath)
+	const metadata = await image.metadata()
+	if (!metadata || !metadata.height || !metadata.width)
+		throw `Could not fetch image metadata at path [${imagePath}]`
+	const imageRatio = metadata.width / metadata.height    
+
+	return imageRatio
 }
